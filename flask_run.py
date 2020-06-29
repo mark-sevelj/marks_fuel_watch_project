315968649049fc01f4922974e4c726ba4b9322a3
= <mark.sevelj@dunwright.com.au>
2020-06-29 19:38:27 +0800

    Added About Page

--- flask_run.py
+++ flask_run.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About Hello World'
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
