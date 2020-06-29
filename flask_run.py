dfc6e0b959a5cb8f3c29fbb50c3f563870301b9a
= <mark.sevelj@dunwright.com.au>
2020-06-29 20:09:43 +0800

    Add fuel watch

--- flask_run.py
+++ flask_run.py
@@ -13,7 +13,21 @@ def about():
 
 @app.route('/fuel_watch')
 def fuel_watch():
-    return 'Fuel Watch Page'
+    return '<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="icon" type="image/png" href="../static/media/favicon-16x16.png" />
+    <title>
+        Fuel Watch
+    </title>
+    <img src="https://www.fuelwatch.wa.gov.au/fuelwatch/art/fuelwatch-logo.gif?
+    pfdrid_c=true" alt="../static/media/fuel_watch_logo.PNG"
+     height="50" width="70">
+
+</head>'
 
 
 
