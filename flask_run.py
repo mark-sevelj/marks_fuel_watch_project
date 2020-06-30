e0ccc0bb89a6fa5fb5a47c23a275f27af46e31e4
= <mark.sevelj@dunwright.com.au>
2020-06-30 18:57:21 +0800

    bug fix

--- flask_run.py
+++ flask_run.py
@@ -3,38 +3,17 @@ from fuel_watch_frontend import generate_fuel_watch_html_string
 
 app = Flask(__name__)
 
-kwargs = {"Region": '',
-          "Suburb": '',
-          "Product": 'Unleaded Petrol',
-          "Day": '',
-          "Surrounding": 'yes',
-          "Brand": '',
-          "Sort_on": 'price',
-          "keys_of_interest": [
-              'updated',
-              'price',
-              'trading-name',
-              'brand',
-              'address',
-              'location',
-              ],
-          }
-
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def fuel_watch():
+    #return generate_fuel_watch_html_string(filtered=True, **kwargs)
+    return "fuel watch"
 
 
 @app.route('/about')
 def about():
     return 'About Hello World'
 
-
-@app.route('/fuel_watch')
-def fuel_watch():
-    #return generate_fuel_watch_html_string(filtered=True, **kwargs)
-    return "fuel watch"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
