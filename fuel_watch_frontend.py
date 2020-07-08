e1f968f3693531b81f7ae6325080bb4370b0a543
= <mark.sevelj@dunwright.com.au>
2020-07-08 20:20:19 +0800

    Moved CSS to static/main.css and added favicon. css is commented out in frontend

--- fuel_watch_frontend.py
+++ fuel_watch_frontend.py
@@ -14,214 +14,225 @@ def html_head():
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="icon" type="image/png" href="../static/media/favicon-16x16.png" />
+    <link rel="icon" type="image/png" href="static/media/favicon-16x16.png" />
+    <link rel="stylesheet" href="static/main.css">
     <title>
-       Marks Fuel Watch Project
+        Marks Fuel Watch Project
     </title>
+
 </head>
     '''
     return head
 
 
-def html_style():
-    """Returns a webpage styling'
-    """
-    style = '''
-<style>
-    h1 {
-         background: black;
-         color: white;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    }
+# def html_style():
+#     """Returns a webpage styling'
+#     """
+#     style = '''
+# <style>
+#     .footer {
+#         position: fixed;
+#         left: 0;
+#         bottom: 0;
+#         width: 100%;
+#         background-color: white;
+#         color: black;
+#         text-align: center;
+#         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#         font-size: 12px;
+#     }
 
-    h2 {
-         background: #b1cdcd;
-         border-radius: 10px;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         font-size: 13px;
-         padding: 10px;
-         text-align: centre;
+#     h1 {
+#          background: black;
+#          color: white;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#     }
 
-    }
+#     h2 {
+#          background: #b1cdcd;
+#          border-radius: 10px;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          font-size: 13px;
+#          padding: 10px;
+#          text-align: centre;
 
-    input {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 5px;
-         text-align: left;
+#     }
 
-    }
+#     input {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 5px;
+#          text-align: left;
 
-    input.select {
-         border-radius: 10px;
-         background-color: #FCF3CF;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 5px;
-         text-align: left;
+#     }
 
-    }
+#     input.select {
+#          border-radius: 10px;
+#          background-color: #FCF3CF;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 5px;
+#          text-align: left;
 
-    label {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 5px;
-         text-align: left;
+#     }
 
-    }
+#     label {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 5px;
+#          text-align: left;
 
-    label.tomorrow {
-        border-radius: 10px;
-        font-size: 16px;
-        background-color: #F2D7D5;
-    }
+#     }
+
+#     label.tomorrow {
+#         background-color: #F2D7D5;
+#         border-radius: 10px;
+#         font-size: 16px;
+#         padding: 5px;
+#     }
 
 
-    nav {
-         background-color: #FCF3CF;
-         border-radius: 10px;
-         padding: 10px;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    }
+#     nav {
+#          background-color: #FCF3CF;
+#          border-radius: 10px;
+#          padding: 10px;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#     }
 
-    p.l {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 10px;
-         text-align: left;
+#     p {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 10px;
+#          text-align: left;
 
-    }
+#     p.l {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 10px;
+#          text-align: left;
 
-    p.c {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 10px;
-         text-align: left;
+#     }
 
-    }
-    p.r {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 5px;
-         text-align: left;
+#     p.c {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 10px;
+#          text-align: left;
 
-    }
+#     }
+#     p.r {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 5px;
+#          text-align: left;
 
-    p.tomorrow {
-        font-size: 12px;
-        background-color: #F2D7D5;
-    }
+#     }
 
-    select {
-         background: white;
-         color: black;
-         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-         padding: 5px;
-         text-align: left;
+#     p.tomorrow {
+#         font-size: 12px;
+#         background-color: #F2D7D5;
+#     }
 
-    }
+#     select {
+#          background: white;
+#          color: black;
+#          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#          padding: 5px;
+#          text-align: left;
 
-    table {
-        border-collapse: collapse;
-        border: 1px solid black;
-        border-radius: 10px;
-        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-        padding: 2px;
-    }
+#     }
 
-    th, td {
-        border: 1px solid black;
-        padding: 15px;
-        border-bottom: 1px solid #ddd;
-    }
+#     table {
+#         border-collapse: collapse;
+#         border: 1px solid black;
+#         border-radius: 10px;
+#         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+#         padding: 2px;
+#     }
 
-    th {
-        background-color: #FCF3CF;
-        color: black;
-        text-align: centre;
-        padding: 15px;
-    }
+#     th, td {
+#         border: 1px solid black;
+#         padding: 15px;
+#         border-bottom: 1px solid #ddd;
+#     }
 
-    tr.today {
-        font-size: 12px;
-        background-color: #F2F3F4;
+#     th {
+#         background-color: #FCF3CF;
+#         color: black;
+#         text-align: centre;
+#         padding: 15px;
+#     }
 
-    }
+#     tr.today {
+#         font-size: 12px;
+#         background-color: #F2F3F4;
 
-    tr.tomorrow {
-        font-size: 12px;
-        background-color: #F2D7D5;
-    }
+#     }
 
-    /*tr:nth-child(even) {background-color: #f5f5f5 ;}*/
+#     tr.tomorrow {
+#         font-size: 12px;
+#         background-color: #F2D7D5;
+#     }
 
-    tr:hover {background-color: #E8F8F5;}
+#     /*tr:nth-child(even) {background-color: #f5f5f5 ;}*/
 
-    /* unvisited link */
-    a:link {
-        color: #273746;
-    }
+#     tr:hover {background-color: #E8F8F5;}
 
-    /* visited link */
-    a:visited {
-        color: #1D8348;
-    }
+#     /* unvisited link */
+#     a:link {
+#         color: #273746;
+#     }
 
-    /* mouse over link */
-    a:hover {
-        color: hotpink;
-    }
+#     /* visited link */
+#     a:visited {
+#         color: #1D8348;
+#     }
 
-    /* selected link */
-    a:active {
-        color: blue;
-    }
+#     /* mouse over link */
+#     a:hover {
+#         color: hotpink;
+#     }
 
-    ul.nav {
-          list-style-type: none;
-          margin: 0;
-          padding: 0;
-          overflow: hidden;
-          background-color: #333;
-    }
+#     /* selected link */
+#     a:active {
+#         color: blue;
+#     }
 
-    li.nav {
-      float: left;
-    }
+#     ul.nav {
+#           list-style-type: none;
+#           margin: 0;
+#           padding: 0;
+#           overflow: hidden;
+#           background-color: #333;
+#     }
 
-    li.nav a {
-      display: block;
-      color: white;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-    }
+#     li.nav {
+#       float: left;
+#     }
 
-    /* Change the link color to #111 (black) on hover */
-    li.nav a:hover {
-      background-color: #111;
-    }
+#     li.nav a {
+#       display: block;
+#       color: white;
+#       text-align: center;
+#       padding: 14px 16px;
+#       text-decoration: none;
+#     }
 
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: white;
-    color: black;
-    text-align: center;
-    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-    font-size: 12px;
-    }
-</style>
-    '''
-    return style
+#     /* Change the link color to #111 (black) on hover */
+#     li.nav a:hover {
+#       background-color: #111;
+#     }
+
+# </style>
+#     '''
+#     return style
 
 
 def html_home_navbar():
@@ -365,16 +376,16 @@ def html_about_body():
         <hr>
         <br>
         <div>
-            <p>
+            <p class='l'>
                 This project is a result of a Learn Python Course developed by
                 Robin Chew.
             </p>
-            <p>
+            <p class='l'>
                The course was run for 10 Weeks from march 2020 at the Bentley
                Technology centre in Perth Western Australia, there was several
                break due to the covid 19 pandemic.
             </p>
-            <p>
+            <p class='l'>
                The objective of the course was to get the Perth Fuel Watch RSS
                feed, extract the data into a format that suited our
                own needs, then present the data in a html page using Flask.
@@ -415,14 +426,15 @@ def generate_home_html_string(filtered=True, **kwargs):
     # Put the suburb name into the correct format
     kwargs['Suburb'] = str.capitalize(kwargs['Suburb'])
 
-    return html_head() + html_style() + html_home_navbar() + html_home_body(filtered, **kwargs) + html_footer()
+    return html_head() + html_home_navbar() + html_home_body(filtered, **kwargs) + html_footer()
 
 
 def generate_about_html_string():
     """
     """
-    return html_head() + html_style() + html_about_navbar() + html_about_body() + html_footer()
+    return html_head() + html_about_navbar() + html_about_body() + html_footer()
 
+# + html_style()
 
 if __name__ == '__main__':
     """Run the fuel watch html generator with default filters
